a5d989577bbd05e9532d869701bd73d3cd643f06
un_satmarean@yahoo.com <un_satmarean@yahoo.com@6c86b803-76bb-78fb-9cb6-0593b93f9c0d>
2011-02-17 23:21:04 +0000

    - removed tool bar - added class "InternallPalleteWindowsToolbar" to serve as tool bar - added class ShapesExample - added hard coded Shape

--- trunk/src/main/java/gui/MoonWorkspaceInternalFrame.java
+++ trunk/src/main/java/gui/MoonWorkspaceInternalFrame.java
@@ -3,9 +3,18 @@ package gui;
 import gov.nasa.worldwind.Model;
 import gov.nasa.worldwind.WorldWind;
 import gov.nasa.worldwind.avlist.AVKey;
+import gov.nasa.worldwind.geom.LatLon;
+import gov.nasa.worldwind.geom.Position;
+import gov.nasa.worldwind.layers.LayerList;
+import gov.nasa.worldwind.layers.RenderableLayer;
+import gov.nasa.worldwind.render.BasicShapeAttributes;
+import gov.nasa.worldwind.render.ExtrudedPolygon;
+import gov.nasa.worldwind.render.Material;
+import gov.nasa.worldwind.render.ShapeAttributes;
 import gov.nasa.worldwind.util.StatusBar;
 
 import java.awt.BorderLayout;
+import java.util.ArrayList;
 
 import javax.swing.JInternalFrame;
 import javax.swing.JPanel;
@@ -13,7 +22,7 @@ import javax.swing.JPanel;
 public class MoonWorkspaceInternalFrame extends JInternalFrame {
 	static int openFrameCount = 0;
 	static final int xOffset = 30, yOffset = 30;
-	static gov.nasa.worldwind.awt.WorldWindowGLCanvas worldWindowGLCanvas;
+	public static gov.nasa.worldwind.awt.WorldWindowGLCanvas wwGLCanvas;
 	
 	public MoonWorkspaceInternalFrame() {
 		    super("New workspace " + (++openFrameCount),
@@ -24,35 +33,46 @@ public class MoonWorkspaceInternalFrame extends JInternalFrame {
 		    
 	        // se creaza canvasul ptr luna
 	    	System.out.println("Creating Moon");
-			worldWindowGLCanvas = new gov.nasa.worldwind.awt.WorldWindowGLCanvas();
+			wwGLCanvas = new gov.nasa.worldwind.awt.WorldWindowGLCanvas();
 
 	    	Model m = (Model) WorldWind.createConfigurationComponent(AVKey.MODEL_CLASS_NAME);
 	    	
 			//initializam Canvasul ptr Luna
-		    worldWindowGLCanvas.setModel(m); //adauga model-ul la canvas
+		    wwGLCanvas.setModel(m); //adauga model-ul la canvas
 		    
 		    
 		    
 		    this.setSize(480, 320);
 		    this.setLocation(xOffset*openFrameCount, yOffset*openFrameCount);
 		    JPanel jPanel=new JPanel(new BorderLayout());
-		    jPanel.add(worldWindowGLCanvas);
+		    jPanel.add(wwGLCanvas);
 		    StatusBar status=new StatusBar();
 		    jPanel.add(status,BorderLayout.SOUTH);
 		    getContentPane().add(jPanel);
 	    	System.out.println("Moon Canvas added");
+	    	
+	    	new ShapesExample(this);
+	    	wwGLCanvas.redrawNow();
+	    	
 			BaseFrame.desktop.add(this);
 
 			this.setVisible(true);
 		    try {
 		    	this.setSelected(true);
-		    } catch (java.beans.PropertyVetoException e) {}
-		    
-		    
-		    
+		    } catch (java.beans.PropertyVetoException e) {} 
+		}
 	
+	
+	
+	public static LayerList getStuff(){
+	LayerList layers = wwGLCanvas.getModel().getLayers();
+	return layers;
 	}
-
+	
+	
+	
+	
+	
 	
 
 }
