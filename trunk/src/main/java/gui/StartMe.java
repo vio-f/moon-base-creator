3884eb6c59f76f9c519b0870df6ee80986ccbc4b
un_satmarean@yahoo.com <un_satmarean@yahoo.com@6c86b803-76bb-78fb-9cb6-0593b93f9c0d>
2011-03-29 12:58:27 +0000

    fixed some log-outs removed duplicate ww jar from build path

--- trunk/src/main/java/gui/StartMe.java
+++ trunk/src/main/java/gui/StartMe.java
@@ -12,15 +12,15 @@ public class StartMe {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		StartMe thisClass = new StartMe();
-		MyLogger.info(thisClass, "App started");
+		
+		MyLogger.getLogger().info("App started");
 		//setarile necesare ptr vizualizarea Lunii
 		
 		Configuration.setValue(AVKey.GLOBE_CLASS_NAME, gov.nasa.worldwind.globes.Moon.class.getName());// seteaza tipul astrului care urmeaza a fi vizualiza
         Configuration.setValue(AVKey.MOON_ELEVATION_MODEL_CONFIG_FILE, "config/Moon/MoonElevationModel.xml");//seteaza  ce fisier de elevatie (inaltimi) sa se foloseasca
         Configuration.setValue(AVKey.LAYERS_CLASS_NAMES, BaseFrame.LAYERS);// LAYERS a fost definit mai sus
         Configuration.setValue(AVKey.INITIAL_ALTITUDE, 600e3);  // 6000km
-        MyLogger.info(thisClass, "Settings made");
+        MyLogger.getLogger().info("Settings made");
 		new BaseFrame();
 
 	}
