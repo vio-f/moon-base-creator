7209990c7f01bd208fb12d7f62f529aeaad3fbe9
viorel.florian <viorel.florian@6c86b803-76bb-78fb-9cb6-0593b93f9c0d>
2011-05-06 16:25:23 +0000

    MyLogger adjusted to print to Info panel also

--- src/main/java/utility/MyLogger.java
+++ src/main/java/utility/MyLogger.java
@@ -3,6 +3,8 @@
  */
 package utility;
 
+import gui.InfoPanel;
+
 import java.net.URL;
 
 import javax.sound.midi.MidiDevice.Info;
@@ -15,69 +17,90 @@ import org.apache.log4j.PropertyConfigurator;
  */
 public class MyLogger {
 
-	private static final String LOG_FILENAME = "log.properties";
+  private static final String LOG_FILENAME = "log.properties";
 
-	/** */
-	private static org.apache.log4j.Logger log;
+  /** */
+  private static org.apache.log4j.Logger log;
 
-	/**
-	 * 
-	 * @return
-	 */
-	public static org.apache.log4j.Logger getLogger() {
+  /**
+   * 
+   * @return
+   */
+  public static org.apache.log4j.Logger getLogger() {
 
-		if (log == null) {
-			log = org.apache.log4j.Logger.getLogger(MyLogger.class);
-			URL logURL = MyLogger.class.getResource("log.properties");
-			PropertyConfigurator.configure(logURL);
-		}
-		return log;
-	}
-
-	/**
-	 * 
-	 * @param obj
-	 * @return
-	 */
-	private static String getFormatedMessage(Object obj, Object message) {
-		return " [" + obj.getClass().getSimpleName().trim() + "] - " + message;
-	}
-
-	/**
-	 * 
-	 * @param source
-	 * @param text
-	 */
-	public static void info(Object source, String text) {
-		getLogger().info(getFormatedMessage(source, text));
-	}
-	
-	/**
-	 * 
-	 * @param source
-	 * @param text
-	 */
-	public static void error(Object source, String text) {
-		getLogger().error(getFormatedMessage(source, text));
-	}
-	
-	/**
-	 * 
-	 * @param source
-	 * @param text
-	 */
-	public static void error(Object source, String text, Throwable t) {
-		getLogger().error(getFormatedMessage(source, text), t);
-	}
-	
-	/**
-     * 
-     * @param source
-     * @param text
-     */
-    public static void error(Object source, Throwable t) {
-        getLogger().error(getFormatedMessage(source, t.getMessage()), t);
+    if (log == null) {
+      log = org.apache.log4j.Logger.getLogger(MyLogger.class);
+      URL logURL = MyLogger.class.getResource("log.properties");
+      PropertyConfigurator.configure(logURL);
     }
-	
-	
+    return log;
+  }
+
+  /**
+   * 
+   * @param obj
+   * @return
+   */
+  private static String getFormatedMessage(Object obj, Object message) {
+    return " [" + obj.getClass().getSimpleName().trim() + "] - " + message;
+  }
+
+  /**
+   * 
+   * @param source
+   * @param text
+   */
+  public static void info(Object source, String text) {
+    String t = getFormatedMessage(source, text);
+    getLogger().info(t);
+    appendToMbcOutput(t);
+  }
+
+  /**
+   * 
+   * @param source
+   * @param text
+   */
+  public static void error(Object source, String text) {
+    String t = getFormatedMessage(source, text);
+    getLogger().error(t);
+    appendToMbcOutput(t);
+  }
+
+  /**
+   * 
+   * @param source
+   * @param text
+   * @param t 
+   */
+  public static void error(Object source, String text, Throwable t) {
+    String ft = getFormatedMessage(source, text);
+
+    getLogger().error(ft, t);
+    appendToMbcOutput(ft);
+
+  }
+
+  /**
+   * 
+   * @param source
+   * @param t 
+   */
+  public static void error(Object source, Throwable t) {
+    String ft = getFormatedMessage(source, t.getMessage());
+    getLogger().error(getFormatedMessage(source, t.getMessage()), t);
+    appendToMbcOutput(ft);
+  }
+
+  /**
+   * appends text to the info panel
+   * @param newTxt 
+   */
+  private static void appendToMbcOutput(String newTxt) {
+    String oldtxt = InfoPanel.getMbcOutput().getText();
+    InfoPanel.getMbcOutput().setText(oldtxt + "\n" + newTxt);
+
+  }
+
+  // EOF
 }
